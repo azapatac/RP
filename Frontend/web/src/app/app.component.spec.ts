614d29f539a7763ff4d186d501c5208c91fc03bc
Andres Zapata <maurozapata17@gmail.com>
2021-01-19 11:08:24 -0500

    Rename proyect

--- Frontend/web/src/app/app.component.spec.ts
+++ Frontend/web/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RP'`, () => {
+  it(`should have as title 'web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RP');
+    expect(app.title).toEqual('web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('RP app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('web app is running!');
   });
 });
